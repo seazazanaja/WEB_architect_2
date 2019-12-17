841baa5234befa2425c59c41e47b3fab65c8c2fe
5ylar <forzepmono@gmail.com>
2019-12-17 23:53:49 +0700

    init

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+function openMenu() {
+    document.getElementById("hamburger").classList.toggle("nav-is-active");
+    document.getElementById("menuMobile").classList.toggle("nav-show-menu");
+    document.getElementById("menuMobile").classList.toggle("nav-hide-menu");
+    document.getElementById("navbar").classList.toggle("position-fixed");
+}
